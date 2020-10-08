382cb9ea9870f70ade7d813058a4567634a8ca81
aidilchairi <72537689+aidilchairi@users.noreply.github.com>
2020-10-08 15:25:40 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,4 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Selalu Tersenyum" },
-    { author: "antjedtta_", quotes: "Walau kadang Hati terluka" },
-    { author: "standinshd", quotes: "Bakal ada hari dimana kita bahagia" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Aidil Chairi W", quotes: "Pgn ngirim ke seseorang, tapi ga punya. SadBoy :'(" },
 ];
